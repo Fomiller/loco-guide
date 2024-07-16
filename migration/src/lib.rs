28a5bbb7c4639daa3fd5486193470e892dad1c7f
Forrest Miller <forrestmillerj@gmail.com>
2024-07-15 20:39:51 -0500

    loco guide

---
+++ migration/src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(elided_lifetimes_in_paths)]
+#![allow(clippy::wildcard_imports)]
+pub use sea_orm_migration::prelude::*;
+
+mod m20220101_000001_users;
+mod m20231103_114510_notes;
+
+mod m20240716_010639_articles;
+mod m20240716_012350_comments;
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(m20220101_000001_users::Migration),
+            Box::new(m20231103_114510_notes::Migration),
+            Box::new(m20240716_010639_articles::Migration),
+            Box::new(m20240716_012350_comments::Migration),
+        ]
+    }
+}
---
+++ src/controllers/mod.rs
@@ -0,0 +1,10 @@
+pub mod auth;
+pub mod notes;
+pub mod user;
+
+pub mod guide;
+
+pub mod home;
+
+pub mod articles;
+pub mod comment;
